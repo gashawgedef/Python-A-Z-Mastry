08f9a334007fa95420aa6b4a0de0b2b1b169b6e0
ggashaw <ggashaw@HQ-ITSD-P007>
2026-01-10 13:21:51 +0300

    master day 1

---
+++ 30- day master/day1.py
@@ -0,0 +1,20 @@
+x = 41
+y = [1,2,"Gashaw Gedef"]
+
+# print(id(y))
+# print(type(y))
+# print(y)
+
+a = 10
+b = a
+a += 1
+
+
+print(id(a))
+print(type(a))
+print(a)
+print(id(b))
+print(type(b))
+print(b)
+
+print(b) 
