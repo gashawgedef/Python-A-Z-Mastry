9bfa2fa36b6d6daa247605fbbf1509df3516fb83
ggashaw <ggashaw@HQ-ITSD-P007>
2026-03-07 11:27:44 +0300

    updates

--- 02-Data-Structures/Lists/freecourselist.py
+++ 02-Data-Structures/Lists/freecourselist.py
@@ -32,3 +32,30 @@ print(myDict)
 
 myDicts ={}
 print(myDicts)
+
+dicts ={"one":1,"two":2}
+dicts["two"] ="Two"
+dicts["three"] =3
+print(dicts)
+print(dicts["one"])
+
+
+
+def traverseDict(dict):
+    for key in dict:
+        print(key,dict[key])
+
+def searchValue(dict,value):
+    for key in dict:
+        if dict[key] ==value:
+            return key,value
+    return "Value not found"
+print(searchValue(dicts,"3"))
+traverseDict(dicts)
+
+newDicts ={}.fromkeys([1,2,"hh"],7)
+print(newDicts)
+
+
+squares = {x:x*x for x in range(5)}
+print(squares)
